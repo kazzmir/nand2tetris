311ced5c7f7772c87a5c389d78db4b7a9aa2db97
Jon Rafkind <jon@rafkind.com>
2020-05-23 23:55:13 -0700

    11: add codegen test

---
+++ projects/10/compiler/cmd/compiler/codegen_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "testing"
+    "strings"
+)
+
+func filterEmpty(values []string) []string {
+    var out []string
+
+    for _, line := range values {
+        if strings.TrimSpace(line) != "" {
+            out = append(out, line)
+        }
+    }
+
+    return out
+}
+
+func doCodeGen(text string) ([]string, error) {
+    ast, err := parse(strings.NewReader(text))
+    if err != nil {
+        return nil, err
+    }
+
+    var out strings.Builder
+
+    err = GenerateCode(ast, &out)
+    if err != nil {
+        return nil, err
+    }
+
+    /* there will be an extra blank line at the end, so we filter it out */
+    return filterEmpty(strings.Split(out.String(), "\n")), nil
+}
+
+func compareCode(actual []string, expected []string) bool {
+    if len(actual) != len(expected) {
+        return false
+    }
+
+    for i, actualLine := range actual {
+        if actualLine != expected[i] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func TestSimpleLet(test *testing.T){
+    text := `class x {
+    function void foo() {
+        var int x, y;
+        let y = 1;
+        let x = y;
+        return;
+    }
+}
+`
+    generated, err := doCodeGen(text)
+    if err != nil {
+        test.Fatalf("could not generate code: %v", err)
+    }
+
+    expected := []string{
+        "function x.foo 2",
+        "push constant 1",
+        "pop local 1",
+        "push local 1",
+        "pop local 0",
+        "push constant 0",
+        "return",
+    }
+
+    if !compareCode(generated, expected) {
+        test.Fatalf("unexpected generated code: actual %v vs expected %v\n", generated, expected)
+    }
+}
+
